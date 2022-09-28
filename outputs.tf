3b0dadf3c9cd62879f4caf5a219f67bf4495f260
James Woolfenden <james.woolfenden@gmail.com>
2022-09-28 17:01:48 +0100

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "instance" {
-  value = aws_lightsail_instance.example
+  value = aws_lightsail_instance.pike
 }
+
+output "public_ports" {
+  value = aws_lightsail_instance_public_ports.pike
+}
+
+
